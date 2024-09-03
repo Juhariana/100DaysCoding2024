7fd76e61f0ceda32ae9c21fa95078df89b658875
Juhariana <juharianaana@gmail.com>
2024-09-03 20:10:56 +0800

    Day2
    Menampilan variabel

---
+++ Day2.java
@@ -0,0 +1,19 @@
+package Codingday2;
+
+public class Main {
+
+		public static void main(String[] args) {
+		// membuat Variabel
+		String nama, alamat, telpon;
+		
+		//Mengisi Variabel
+		nama = "juhariana" ;
+		alamat = "tinambung" ;
+		telpon = "083103495723" ;
+		 // mencetak variabel kelayar 
+		 System.out.println("nama " + nama ) ;
+	     System.out.println("alamat " + alamat ) ;
+		System.out.println("telpon " + telpon ) ;
+	}
+	
+}
