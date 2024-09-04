f9680dc1869cf3509e296c45424841ff7163369b
Juhariana <juharianaana@gmail.com>
2024-09-04 19:05:27 +0800

    Day3
    Mengkreasikan sout

---
+++ Day3.java
@@ -0,0 +1,20 @@
+package codingday3 ;
+
+public class Main {
+	public static void main(String[] args) {
+		// Membuat Variabel
+		String nama, alamat, prodi, fakultas ;
+		
+		//Mengisi Variabel 
+		nama = "juhariana" ;
+		alamat = "tinambung" ;
+		prodi = "informatika" ;
+		fakultas = "teknik" ;
+		
+		// Mencetak Variabel kelayar
+		System.out.println("nama ; " + nama ) ;        System.out.println("alamat; " + alamat ) ;
+    	System.out.println("prodi ; " + prodi ) ;
+		System.out.println("fakultas ; " + fakultas ) ;
+			
+	}
+}
