89470edbbe7f347a5a8e38cc9dc0bbf16b9893af
Juhariana <juharianaana@gmail.com>
2024-09-05 21:03:15 +0800

    Day4
    Belajar println dan printf

---
+++ CodingDay4.java
@@ -0,0 +1,10 @@
+package codingday4 ;
+public class Main {
+	public static void main(String[] args) {
+		System.out.println ("nama : juhariana") ;
+		System.out.println ("kelas : D informatika") ;
+		
+		System.out.printf ("tinggi badan saya  %d" , 153) ;
+		
+	}
+}
