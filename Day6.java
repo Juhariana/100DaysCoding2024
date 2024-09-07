e2884c48c8f66ae8acb8d3aa21ede89ed396fe7d
Juhariana <juharianaana@gmail.com>
2024-09-07 19:18:03 +0800

    Day6
    Membuat deklarasi

---
+++ Day6.java
@@ -0,0 +1,15 @@
+package day6 ;
+public class Main {
+	public static void main(String[] args) {
+	
+		// membuat deklarasi
+		
+		int foto,video ;
+		 
+		 	foto = 10 ;
+		 	video = 30 ;
+		 	
+		 System.out.printf("jumlah foto = %d /n" ,foto) ;
+		 System.out.printf("jumlah video = %d /n" ,video) ;			
+	}
+}
