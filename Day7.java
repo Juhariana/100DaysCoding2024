eff2f09283696e15b0cf9fa8a09cbc08d6fc1e64
Juhariana <juharianaana@gmail.com>
2024-09-08 16:47:57 +0800

    Day7
    Belajar inisialisasi variabel

---
+++ Day7.java
@@ -0,0 +1,12 @@
+package day7 ;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// inisialisasi variabel
+		String nama = ("juhariana") ;
+		int umur = 19 ;
+		System.out.printf("Nama : %s umur : %d " ,nama,umur) ;
+	}
+}
