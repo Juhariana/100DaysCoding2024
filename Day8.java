76a4d62400c3db66b596cf5d73dd0072fa5e65cf
Juhariana <juharianaana@gmail.com>
2024-09-09 20:05:48 +0800

    Day8
    Belajar variabel:update

---
+++ Day8.java
@@ -0,0 +1,13 @@
+public class codingday8 {
+	
+	public static void main(String[] args) {
+	   
+	   System.out.println ("Belajar update variabel") ;
+	        
+	        String nama = "dede" ;
+	      System.out.println("nama : " + nama);
+		nama = "juha" ;
+	 nama += "riana" ;
+		System.out.println ("nama setelah update: " + nama);
+	}
+}
