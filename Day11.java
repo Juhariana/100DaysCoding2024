d16b6bc20635e4aab14e12767aced80ecf9b2608
Juhariana <juharianaana@gmail.com>
2024-09-12 20:57:16 +0800

    Tipe data short

---
+++ Day11.java
@@ -0,0 +1,12 @@
+public class day11 {
+	
+	public static void main(String[] args) {
+		
+		/* belajar menggunakan 
+		tipe data short*/
+		
+		short tahun = 2024 ;
+		
+		System.out.println("tahun = " + tahun );
+	}
+}
