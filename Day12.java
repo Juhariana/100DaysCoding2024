c274bd0a76a5ea942e643046fd1663ca7473e026
Juhariana <juharianaana@gmail.com>
2024-09-13 21:07:13 +0800

    Tipe data int

---
+++ Day12.java
@@ -0,0 +1,13 @@
+package day12 ;
+
+public class day12 {
+    public static void main(String[] args) {
+        // Membuat varibel tipe data int
+        int tanggal = 07;
+        int bulan = 05 ;
+        
+        System.out.println("tanggal :"+ tanggal);
+        System.out.println("bulan :" + bulan);
+    }
+    
+}
