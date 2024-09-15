be6a71aefa15bbc055860fcb31000eeccc10566e
Juhariana <juharianaana@gmail.com>
2024-09-15 14:36:34 +0800

    Tipe data : long

---
+++ Day14.java
@@ -0,0 +1,13 @@
+public class day14 {
+    
+    public static void main(String[] args) {
+        
+        // Membuat variabel tipe data long
+        
+        long coding = 100 ;
+        
+        System.out.println("coding :"+ coding);
+        
+    }
+    
+}
