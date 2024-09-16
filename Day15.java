c0924025f8f32ee6cf7fb0d1fa9d53b0b40ef95c
Juhariana <juharianaana@gmail.com>
2024-09-16 10:47:03 +0800

    Tipe data double

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class day15 {
+    
+    public static void main(String[] args) {
+        
+        // Membuat variabel tipe data double
+        
+        double variabel_double = 15.07 ;
+        
+        System.out.println("double :"+ variabel_double);
+        
+    }
+    
+}
