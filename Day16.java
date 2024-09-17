bcc8864db164bc91e91c374070ad531c29a0fb7a
Juhariana <juharianaana@gmail.com>
2024-09-17 18:36:15 +0800

    Tipe data float

---
+++ Day16.java
@@ -0,0 +1,13 @@
+public class day16 {
+    
+    public static void main(String[] args) {
+        
+        // Membuat variabel tipe data float
+        
+        double variabel_float = 15.0f ;
+        
+        System.out.println("float :"+ variabel_float);
+        
+    }
+    
+}
