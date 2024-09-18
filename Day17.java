b754da54ab78ca48c4b61788ae603475c2fb4417
Juhariana <juharianaana@gmail.com>
2024-09-18 19:12:34 +0800

    Tipe data char

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class day17 {
+    
+    public static void main(String[] args) {
+        
+        // Membuat variabel tipe data char
+        
+        char kelas = 'D' ;
+        
+        System.out.println("kelas:"+ kelas);
+        
+    }
+    
+}
