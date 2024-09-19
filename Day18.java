fa56dd91171a7dedeb831926b4cc96a8505671a4
Juhariana <juharianaana@gmail.com>
2024-09-19 19:40:17 +0800

    Tipe data boolean

---
+++ Day18.java
@@ -0,0 +1,13 @@
+public class day18 {
+    
+    public static void main(String[] args) {
+        
+        /* tipe data boolean , tipe data boolean hanya bisa menampung dua nilai yaitu true dan false*/
+        
+      boolean lulus = true ;
+        
+        System.out.println("lulus:"+ lulus);
+        
+    }
+    
+}
