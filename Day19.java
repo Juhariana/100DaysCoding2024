4b513f63ea241391c866291b60d6521111b90bbd
Juhariana <juharianaana@gmail.com>
2024-09-20 20:29:35 +0800

    Tipe data reference string

---
+++ Day19.java
@@ -0,0 +1,7 @@
+public class day19 {
+	public static void main(String[] args) {
+		// penggunaan  tipe data reference string
+		String panggilan ="dede" ;
+		System.out.println("nama panggilan saya : "+ panggilan) ;
+	}
+}
