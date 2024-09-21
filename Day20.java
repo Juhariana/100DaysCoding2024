b8eab111426e9f072d87b86b16c51d852d0d4f7b
Juhariana <juharianaana@gmail.com>
2024-09-22 06:12:53 +0800

    Tipe data reference byte

---
+++ Day20.java
@@ -0,0 +1,8 @@
+public class Day20 {
+	
+	public static void main(String[] args) {
+		// menggunakan tipe data reference byte
+		Byte tinggi = 153;
+		System.out.println("tinggi badan saya : " + tinngi);
+	}
+}
