7e13058d0d1ee7bf042133d55d6b08a521bc3e56
Juhariana <juharianaana@gmail.com>
2024-09-22 21:27:38 +0800

    Tipe data reference short

---
+++ Day21.java
@@ -0,0 +1,7 @@
+public class day21 {
+	public static void main(String[] args) {
+		// penggunaan  tipe data reference short
+		short Tahun = 2024 ;
+		System.out.println("tahun ini : " + Tahun) ;
+	}
+}
