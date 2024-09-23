468af19ff8c699b386f4175155855de1e1a12d46
Juhariana <juharianaana@gmail.com>
2024-09-23 20:05:56 +0800

    Tipe data reference integer

---
+++ Day22.java
@@ -0,0 +1,7 @@
+public class day22 {
+	public static void main(String[] args) {
+		/*kali ini saya akan menggunakan tipe data reference integer*/
+		Integer umur = 19 ;
+		System.out.println("umur saya tahun ini : " + umur ) ;
+	}
+}
