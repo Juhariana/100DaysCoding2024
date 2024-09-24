cb7ae0f9652a3e21db3f1660769e8091ae8de871
Juhariana <juharianaana@gmail.com>
2024-09-24 22:29:25 +0800

    Tipe data reference long

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class day23 {
+    
+    public static void main(String[] args) {
+        // penggunaan tipe data reference long
+        Long var1 = 200L ;
+        Long var2 = 100L ;
+        Long hasil = var1 * var2 ;
+    System.out.println("Hasil perkalian var1 dan var2 : " + hasil);
+    }
+    
+}
