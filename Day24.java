627a2dec94a92d6ba4b2ad55385e618eea3bac40
Juhariana <juharianaana@gmail.com>
2024-09-25 19:37:44 +0800

    Tipe data reference float

---
+++ Day24.java
@@ -0,0 +1,12 @@
+public class day24 {
+    
+    public static void main(String[] args) {
+  
+              // penggunaan tipe data reference float
+        Float jam = 7.00f ;
+
+        System.out.println("jam menunjukkan pukul : " + jam);
+
+        }
+    
+}
