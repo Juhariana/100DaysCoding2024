7d11d59f4823ee3f2c954e998729f7aa3337449b
Juhariana <juharianaana@gmail.com>
2024-09-26 20:46:51 +0800

    Tipe data reference double

---
+++ Day25.java
@@ -0,0 +1,14 @@
+public class day25 {
+	public static void main(String[] args) {
+		
+		// disini saya menggunakan tipe data reference double
+		
+		Double gula1 = 1.00 ;
+		Double gula2 = 2.00 ;
+		Double jumlah = gula1 + gula2;
+		
+		System.out.println("jumlah gula  setelah ditambah : " + jumlah ) ;
+		
+		
+	}
+}
