4c2b231d21a2680a463f73113df1f0bebcc208f9
Juhariana <juharianaana@gmail.com>
2024-09-27 20:15:43 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,11 @@
+public class day26 {
+public static void main(String[]args) {
+// Menggunakan tipe data reference character 
+
+Character kelas = '1' ;
+
+System.out.println("kelas : " + kelas);
+
+}
+
+}
