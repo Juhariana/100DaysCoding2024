2bd512db96f59734ea179016387b32067bd965fd
Juhariana <juharianaana@gmail.com>
2024-09-29 19:31:41 +0800

    Tipe data reference boolean

---
+++ Day28.java
@@ -0,0 +1,15 @@
+public class day28 {
+    
+    public static void main(String[] args) {
+        
+      System.out.println("menggunakan tipe data reference boolean");
+        
+      Boolean lulus = true ;
+      Boolean tidak_lulus = false ;
+        
+        System.out.println("saya sudah lulus sekolah : "+ lulus);
+        System.out.println("saya tidak lulus sekolah : " + tidak_lulus);
+        
+    }
+    
+        }
