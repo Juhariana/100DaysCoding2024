5478879a726958ce53e3a2b0036a625d4214775c
Juhariana <juharianaana@gmail.com>
2024-09-30 22:54:05 +0800

    Tipe data reference BigInteger

--- Day29.java
+++ Day29.java
@@ -1,6 +1,7 @@
 import java.math.BigInteger ;
 public class Day29 {
-public static void main(String[] args) { BigInteger nilai = new BigInteger ("1000000000000000000000000") ;
+public static void main(String[] args) {
+  BigInteger nilai = new BigInteger ("1000000000000000000000000") ;
 System.out.println("nilai : " + nilai);
   
       }
