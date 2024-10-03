893a0718bc75b4b17ab1344e988481e66f97581e
Juhariana <juharianaana@gmail.com>
2024-10-03 21:08:01 +0800

    Konversi tipe data primitif ke string

---
+++ Day32.java
@@ -0,0 +1,11 @@
+public class day32 {
+
+		public static void main(String[] args) {
+	
+			System.out.println("konversi primitif ke string");
+		
+		int a = 1000;
+		String b= Integer.toString(a);
+		System.out.println("nilai : " + b );
+	}
+}
