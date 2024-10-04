a8fd1f9704025af20f891a261bf08f0b4a1f7b4c
Juhariana <juharianaana@gmail.com>
2024-10-04 21:46:55 +0800

    Type casting

---
+++ Day33.java
@@ -0,0 +1,8 @@
+  public class day33 {
+  public static void main(String[] args) {
+    System.out.println("type casting");
+        int a = 15;
+        double b = a ;  
+        System.out.println(b);  
+    }
+}
