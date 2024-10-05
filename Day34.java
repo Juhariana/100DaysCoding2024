8d9877fa24fecd601bc074313e994ff4df1ff642
Juhariana <juharianaana@gmail.com>
2024-10-05 20:16:45 +0800

    Input dari keyboard

---
+++ Day34.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class day34 {
+ public static void main(String[] args) {    
+     Scanner input = new Scanner(System.in);
+   
+     System.out.print("nama saya : ");
+     String nama = input.nextLine();
+
+     System.out.print("umur saya : ");
+     int umur = input.nextInt();
+   
+        input.close();
+    }
+}
