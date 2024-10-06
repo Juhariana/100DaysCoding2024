f95cc91df21e90fcf0360a126ac80af5d694ff6e
Juhariana <juharianaana@gmail.com>
2024-10-06 20:29:41 +0800

    Operator aritmatika: penjumlahan dan pengurangan

---
+++ Day35.java
@@ -0,0 +1,12 @@
+public class day35 {
+    public static void main(String[] args) {
+       System.out.println("menggunakan operator aritmatika : penjumlahan dan pengurangan");
+        int angka1 = 50;
+        int angka2 =20;
+    
+        int penjumlahan = angka1 + angka2;
+        int pengurangan = angka1 - angka2;
+             System.out.println("Hasil penjumlahan: " + penjumlahan);
+             System.out.println("Hasil pengurangan: " + pengurangan);
+    }
+}
