c403b446b91106e6949b1e807706077346ea1434
Juhariana <juharianaana@gmail.com>
2024-10-07 19:13:48 +0800

    Operator aritmatika: perkalian,pembagian,dan sisa bagi/modulus

---
+++ Day36.java
@@ -0,0 +1,16 @@
+public class day36 {
+    public static void main(String[] args) {
+      System.out.println("operator aritmatika:perkalian,pembagian dan modulus");
+          int a = 50;
+        int b = 20;
+
+        int perkalian = a * b;
+        System.out.println("Hasil Perkalian: " + perkalian);
+
+        int pembagian = a / b;
+        System.out.println("Hasil Pembagian: " + pembagian);
+
+        int modulus = a % b;
+        System.out.println("Hasil Modulus: " + modulus);
+    }
+}
