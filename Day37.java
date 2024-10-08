013dc025718cb0a166c30bb694588ca3de75d872
Juhariana <juharianaana@gmail.com>
2024-10-08 20:52:16 +0800

    Operator penugasan: nilai,tambah dan kurang

---
+++ Day37.java
@@ -0,0 +1,15 @@
+public class day37 {
+	public static void main(String[] args) {
+		System.out.println("operator penugasan:penugasan nilai , pertambahan , dan perkurangan");
+		byte angka ;
+		// pengisian nilai(=)
+		angka = 50;
+		System.out.println("nilai angka : " + angka) ;
+		//penugasan dan penambahan (+=)
+		angka += 30;
+		System.out.println("nilai angka setelah ditambah : " + angka) ;
+		//penugasan dan pengurangan(-=)
+		angka -= 20;
+		System.out.println("nilai angka setelah dikurang : " + angka) ;
+	}
+}
