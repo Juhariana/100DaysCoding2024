d269bc798d035d5784899a24b2b5e1ad6f375836
Juhariana <juharianaana@gmail.com>
2024-10-09 18:36:16 +0800

    Operator penugasan:pengisian perkalian dan pembagian

---
+++ Day38.java
@@ -0,0 +1,16 @@
+public class day38 {
+	public static void main(String[] args) {
+		
+		System.out.println("operator penugasan:pengisian dan perkalian,pengisian dan pembagian");
+		
+		byte angka = 11;
+		System.out.println("nilai angka : " + angka);
+	
+			//penugasan dan perkalian (*=)
+		angka *= 5;
+		System.out.println("nilai angka setelah dikali : " + angka) ;
+		//penugasan dan pembagian(%=)
+		angka %= 2;
+		System.out.println("sisa bagi variabel angka setelah dibagi: " + angka) ;
+	}
+      }
