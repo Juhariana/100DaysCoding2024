f44f7542004eb3e4dd10e7aff4bc09e247a507c8
Juhariana <juharianaana@gmail.com>
2024-10-10 19:40:41 +0800

    Operator penugasan:pengisian dan modulo

---
+++ Day39.java
@@ -0,0 +1,14 @@
+public class day39 {
+	public static void main(String[] args) {
+		
+		System.out.println("operator penugasan:pengisian dan modulus");
+		
+		byte angka = 11;
+		System.out.println("nilai angka : " + angka);
+	
+			//pengisian dan modulus (%=)
+		angka %= 2;
+		System.out.println("sisa bagi nilai angka setelah dibagi : " + angka) ;
+		
+	}
+}
