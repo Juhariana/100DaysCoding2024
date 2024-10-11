7eabc8297afb74dfe58026a29614d83116df4d9c
Juhariana <juharianaana@gmail.com>
2024-10-11 18:41:52 +0800

    Operator perbandingan:lebih kecil dan lebih besar

---
+++ Day40.java
@@ -0,0 +1,13 @@
+public class day40 {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 100;
+
+        // lebih besar(>)
+            System.out.println("a lebih besar dari b"+(a>b));
+      // lebih kecil(<)
+            System.out.println("a lebih kecil dari b"+(a<b));
+        
+        
+    }
+  }
