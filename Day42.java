cbbeedc54531c056f1ed31436a64309fe76a9cb3
Juhariana <juharianaana@gmail.com>
2024-10-13 17:40:39 +0800

    Operator perbandingan sama dengan dan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,14 @@
+public class day42 {
+    public static void main(String[] args) {
+   System.out.println("operator perbandingan");
+        int a = 100;
+        int b = 100;
+
+        // sama dengan(==)
+            System.out.println("a sama dengan b : "+(a == b));
+      // tidak sama dengan
+            System.out.println("a tidak sama dengan b : "+(a != b));
+        
+        
+    }
+}
