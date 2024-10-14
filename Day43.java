fc63ab91f192977eb284b849c3e5576823fb192b
Juhariana <juharianaana@gmail.com>
2024-10-14 18:43:24 +0800

    Operator perbandingan : lebih besar sama dengan & lebih kecil sama dengan

---
+++ Day43.java
@@ -0,0 +1,13 @@
+public class day43 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+
+        // lebih besar sama dengan(>=)
+            System.out.println("a lebih besar sama dengan b : "+(a >= b));
+      // lebih kecil sama dengan(<=)
+            System.out.println("a lebih kecil sama dengan b : "+(a <= b));
+        
+        
+    }
+  }
