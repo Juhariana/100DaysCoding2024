d5e5c222588b633234409a7e4bf1dded347f0136
Juhariana <juharianaana@gmail.com>
2024-10-15 18:47:19 +0800

    Operator logika : AND

---
+++ Day44.java
@@ -0,0 +1,11 @@
+public class day44{
+	public static void main(String[] args) {
+		System.out.println("operator logika : AND");
+		boolean var1 = true;
+		boolean var2 = true;
+		boolean var3 = false;
+		
+		System.out.println( var1 && var2 ); // true
+		System.out.println(var2 && var3 ); // false
+	}
+}
