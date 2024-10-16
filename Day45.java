ca55bcd643f85c0c55600ff63ba776c06177c9d8
Juhariana <juharianaana@gmail.com>
2024-10-16 20:01:23 +0800

    Operator logika : OR

---
+++ Day45.java
@@ -0,0 +1,11 @@
+public class day45 {
+	public static void main(String[] args) {
+  System.out.println("operator logika : OR");	boolean a = false;
+	boolean b = true;
+	boolean c = false;
+	
+	System.out.println(a || b);
+	System.out.println(a || c);
+		
+	}
+}
