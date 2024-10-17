46b40d06ee47d7324b0027154561b7c50fbac224
Juhariana <juharianaana@gmail.com>
2024-10-17 19:10:02 +0800

    Operator logika negasi/kebalikan

---
+++ Day46.java
@@ -0,0 +1,11 @@
+public class day46{
+	public static void main(String[] args) {
+		System.out.println("operator logika");
+		
+		// negasi/kebalikan
+		boolean a = false;
+		System.out.println(!a);//true
+		boolean b = true;
+		System.out.println(!b);//false
+	}
+}
