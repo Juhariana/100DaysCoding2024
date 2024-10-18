aad05bbbda51e19798ccf4632a82f283ec8fa3ec
Juhariana <juharianaana@gmail.com>
2024-10-18 17:21:49 +0800

    Penggunaan if

---
+++ Day47.java
@@ -0,0 +1,12 @@
+public class day47 {
+    public static void main(String[] args) {
+       System.out.println("penggunaan if");
+         int a = 50;
+        int b = 50;
+
+        if (a >=b) {
+            System.out.println("a lebih besar sama dengan b");
+        
+        }
+    }
+}
