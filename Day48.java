e1ec64bb905bf9dd961df4c454e8d5ac26630692
Juhariana <juharianaana@gmail.com>
2024-10-19 18:11:07 +0800

    Create Day48.java

---
+++ Day48.java
@@ -0,0 +1,12 @@
+public class day48{
+	public static void main(String[] args) {
+	System.out.println("percabangan: if-else");
+			int umur = 17;
+
+if (umur <= 16) {
+    System.out.println("Kamu sudah cukup umur");
+} else  {
+    System.out.println("Kamu masih di bawah umur");
+}
+	}
+}
