cdc7a67c4f3fc5b11d21cef6a5ef27aeb202bb2a
Juhariana <juharianaana@gmail.com>
2024-10-19 18:20:22 +0800

    Percabangan : if-else

--- Day48.java
+++ Day48.java
@@ -1,7 +1,7 @@
 public class day48{
-	public static void main(String[] args) {
-	System.out.println("percabangan: if-else");
-			int umur = 17;
+public static void main(String[] args) {
+System.out.println("percabangan: if-else");
+int umur = 17;
 
 if (umur <= 16) {
     System.out.println("Kamu sudah cukup umur");
