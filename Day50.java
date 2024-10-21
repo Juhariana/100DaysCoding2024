a8bd647272a54a8092016b7f845196bc1678fdd3
Juhariana <juharianaana@gmail.com>
2024-10-21 19:08:52 +0800

    Percabangan : if-else if-else

---
+++ Day50.java
@@ -0,0 +1,15 @@
+ public class day50 {
+    public static void main(String[] args) {
+   System.out.println("percabangan : if -else if-else");
+        int a = 50;
+        int b = 20;
+
+        if (a < b) {
+            System.out.println("a lebih kecil dari b");
+        } else if (a > b) {
+            System.out.println("a lebih besar dari b");
+        } else {
+        	System.out.println("a sama dengan b");
+        }
+    }
+ }
