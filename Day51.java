2bc751c61972ec012197dade09bee0fc8010608c
Juhariana <juharianaana@gmail.com>
2024-10-22 18:31:57 +0800

    Switch case

--- Day51.java
+++ Day51.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class day51 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         
         System.out.print("Masukkan  jam (1,6,8) ");
         int jam = scanner.nextInt();
