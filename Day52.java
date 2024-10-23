a7c1e66a94093086a90a468b0f3b6ef57fac75cc
Juhariana <juharianaana@gmail.com>
2024-10-23 21:06:38 +0800

    Ternary

--- Day52.java
+++ Day52.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class day52 {
     public static void main(String[] args) {
-      System.out.println("ternary");
-      Scanner dede = new Scanner(System.in);
+      System.out.println("Ternary");
+        Scanner dede = new Scanner(System.in);
 
         System.out.print("Masukkan jam (0-23): ");
-        int jam = scanner.nextInt();
+        int jam = dede.nextInt();
+
         String waktu = (jam >= 0 && jam < 12) ? "Pagi" : 
                        (jam >= 12 && jam < 18) ? "Siang" : 
                        (jam >= 18 && jam <= 23) ? "Malam" : "Jam tidak valid";
 
         System.out.println("Sekarang: " + waktu);
 
-        scanner.close(); 
+        dede.close(); 
     }
 }
