362820ae3179e3ee901a892e2f69c436b1cbed05
Juhariana <juharianaana@gmail.com>
2024-10-24 19:23:29 +0800

    Looping:for

---
+++ Day53.java
@@ -0,0 +1,8 @@
+public class day53 {
+    public static void main(String[] args) {
+        System.out.println("looping:for");
+        for (byte angka = 1; angka <= 100; angka++) {
+            System.out.println("angka ke-" + angka);
+        }
+    }
+}
