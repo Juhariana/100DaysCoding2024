44295e36bdbf8b65e55a09fc9029c04df2fd2e78
Juhariana <juharianaana@gmail.com>
2024-10-25 20:34:53 +0800

    Looping:for increment

---
+++ Day54.java
@@ -0,0 +1,8 @@
+public class day54 {
+  public static void main(String[] args) {
+   System.out.println("looping:for increment");
+       for (int dede=0 ; ; dede++) {
+          System.out.println("hahahahahahahahahahahahahahaha " );
+        }
+    }
+}
