6c77d2de0da7cd53c2644f123129bd60cc2376dc
Juhariana <juharianaana@gmail.com>
2024-10-26 18:45:17 +0800

    looping: for decrement");

---
+++ Day55.java
@@ -0,0 +1,9 @@
+public class day55 {
+    public static void main(String[] args) {
+        System.out.println("looping: for decrement");
+
+        for (int dede = 10; dede >= 0; dede--) {
+            System.out.println(dede);
+        }
+    }
+        }
