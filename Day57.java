6f65b942a36cf43a6042bbab0cddde2c33679a7b
Juhariana <juharianaana@gmail.com>
2024-10-28 21:11:53 +0800

    While increment

---
+++ Day57.java
@@ -0,0 +1,11 @@
+public class day57{
+    public static void main(String[] args) {
+      System.out.println("while increment");
+      byte a = 1;  
+
+        while (a <= 10) {  
+            System.out.println("Nilai a: " + a);
+            a++;  
+        }
+    }
+}
