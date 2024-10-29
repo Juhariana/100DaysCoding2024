f42f42896cebbcab6ac5044be5d69c08c1fc23df
Juhariana <juharianaana@gmail.com>
2024-10-29 18:28:14 +0800

    While decrement

---
+++ Day58.java
@@ -0,0 +1,11 @@
+public class day58{
+    public static void main(String[] args) {
+      System.out.println("while decrement");
+      byte a = 5;  
+
+        while (a >= 1) {  
+            System.out.println("angka : " + a);
+            a--;  
+        }
+    }
+}
