5848e9c8d783d0250f884ba67e350dd604c61898
Juhariana <juharianaana@gmail.com>
2024-10-30 22:14:04 +0800

    For each

---
+++ Day59.java
@@ -0,0 +1,10 @@
+public class day59 {
+	public static void main(String[] args) {
+	System.out.println("for each");
+			boolean[] dee = {false, true, false ,true};
+
+for (boolean dede : dee) {
+    System.out.print(dede + " ");
+}
+	}
+}
