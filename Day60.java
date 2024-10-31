f20e40559b1adadfd9ea16dd89491d0a59bc4e72
Juhariana <juharianaana@gmail.com>
2024-10-31 20:27:26 +0800

    Create Day60.java

---
+++ Day60.java
@@ -0,0 +1,11 @@
+public class day60 {
+    public static void main(String[] args) {
+     System.out.println("do while");
+           int dee= 1;
+
+        do {
+            System.out.println("haii");
+            dee++;
+        } while (dee <= 1 );
+    }
+}
