3009443d96043d00b8b298aa57fb64f68c949ebc
Juhariana <juharianaana@gmail.com>
2024-11-01 19:51:51 +0800

    Infinite loop

---
+++ Day61.java
@@ -0,0 +1,8 @@
+public class day61 {
+    public static void main(String[] args) {
+        System.out.println("infinite loop");
+        while (true) {
+            System.out.println("jeng jeng jeng");
+        }
+    }
+}
