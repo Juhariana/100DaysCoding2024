9318def04d089420bb614e0a0288271072859999
Juhariana <juharianaana@gmail.com>
2024-11-02 20:33:14 +0800

    Keyword break

---
+++ Day62.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("keyword break");
+         for (int dee = 0; dee< 15; dee++) { 
+            if (dee == 5) {
+                break;  
+            }
+            System.out.println(dee);
+        }
+    }
+}
