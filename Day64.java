95576261acebe019df2b3ecafd481f7ab2ff2f34
Juhariana <juharianaana@gmail.com>
2024-11-04 21:53:23 +0800

    Break label

---
+++ Day64.java
@@ -0,0 +1,15 @@
+public class ContohBreakDenganLabel {
+    public static void main(String[] args) {
+        System.out.println("break label");
+        deeLoop: 
+        for (int a = 1; a <= 3; a++) {
+            for (int b = 1; b <= 3; b++) {
+                System.out.println("a = " + a + ", b = " + b);
+                
+                if (a == 2 && b == 2) {
+                    break deeLoop;
+                }
+            }
+        }
+    }
+}
