e452289338582f6bf92a5231864a3709b0ed43e4
Juhariana <juharianaana@gmail.com>
2024-11-08 18:22:18 +0800

    Public non void

---
+++ Day68.java
@@ -0,0 +1,10 @@
+public class day68 {
+     public String dee() {
+        return "haiiii"; 
+    }
+   public static void main(String[] args) {
+        day68 iya = new day68();
+
+       System.out.println(iya.dee()); 
+    }
+}
