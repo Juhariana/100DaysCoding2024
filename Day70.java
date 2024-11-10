c7767ce3c013e2cc6307b5227807f77e430e3ce2
Juhariana <juharianaana@gmail.com>
2024-11-10 20:10:07 +0800

    Method parameter

---
+++ Day70.java
@@ -0,0 +1,10 @@
+public class day70 {
+    public void dee(String nama) {
+        System.out.println("nama: " + nama);
+    }
+
+    public static void main(String[] args) {
+        day70 nama = new day70();
+        nama.dee("juhariana");
+    }
+}
