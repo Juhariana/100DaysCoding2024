0d30e9fc85ec765de878c17a2cd692703bb3dab3
Juhariana <juharianaana@gmail.com>
2024-11-11 19:32:23 +0800

    Method static

---
+++ Day71.java
@@ -0,0 +1,9 @@
+ public class Day71 {
+    public static void nama() {
+        System.out.println("juhariana");
+    }
+    public static void main(String[] args) {
+        Day71.nama();
+         
+    }
+ }
