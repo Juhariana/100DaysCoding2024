84ea0b8b80b044397b9f0eef1127122b53d9b5e9
Juhariana <juharianaana@gmail.com>
2024-11-13 18:15:03 +0800

    String method : charAt

---
+++ Day73.java
@@ -0,0 +1,11 @@
+public class Day73 {
+    public static void main(String[] args) {
+   System.out.println("String Method : CharAt");
+             String nama = "juhariana";
+
+        int indeks = 7;
+        char karakter = nama.charAt(indeks); 
+
+        System.out.println("Karakter pada indeks " + indeks + " adalah: " + karakter);
+    }
+}
