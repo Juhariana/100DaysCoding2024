d5c82f1d240e1a057e06210976c7d05a4b2d9a54
Juhariana <juharianaana@gmail.com>
2024-11-14 17:54:28 +0800

    String method: length

---
+++ Day74.java
@@ -0,0 +1,8 @@
+public class Day74 {
+    public static void main(String[] args) {
+        String nama = "juhariana";
+        int panjang = nama.length();
+        
+        System.out.println("Panjang nama saya adalah : " + panjang);
+    }
+}
