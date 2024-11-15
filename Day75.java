3cde3c27f613e540de5151c43759915c3fd3d369
Juhariana <juharianaana@gmail.com>
2024-11-15 19:07:46 +0800

    String method: toLowerCase

--- Day75.java
+++ Day75.java
@@ -8,14 +8,4 @@ public class Day75 {
         System.out.println("nama saya dalam huruf kapital : " + kapital);//JUHARIANA
         System.out.println("nama saya dalam huruf kecil : " + huruf_kecil);//juhariana
     }
-}public class Day75 {
-    public static void main(String[] args) {
-        
-        String kapital = "JUHARIANA";
-        
-        String huruf_kecil = kapital.toLowerCase();
-        
-        System.out.println("nama saya dalam huruf kapital : " + kapital);//JUHARIANA
-        System.out.println("nama saya dalam huruf kecil : " + huruf_kecil);//juhariana
-    }
 }
