a33cfc22b3115c2c83ccd534492eae34e12543cc
Juhariana <juharianaana@gmail.com>
2024-11-16 17:15:37 +0800

    String method: toUpperCase

---
+++ Day76.java
@@ -0,0 +1,10 @@
+public class Day76 {
+    public static void main(String[] args) {
+        
+        String huruf_kecil  = "juhariana";
+        String kapital = huruf_kecil.toUpperCase();
+        
+        System.out.println("nama saya dalam huruf kecil : " + huruf_kecil);//juhariana
+        System.out.println("nama saya dalam huruf kapital : " + kapital);//JUHARIANA
+     }
+}
