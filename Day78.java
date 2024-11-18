6fbfea8e5c2de9479f1ffe1d332d3194d30272aa
Juhariana <juharianaana@gmail.com>
2024-11-18 19:56:04 +0800

    String method : equalsIgnoreCase

--- Day78.java
+++ Day78.java
@@ -1,8 +1,8 @@
-   public class Day78{
+  public class Day78{
     public static void main(String[] args) {
-        String a = "Dee";
-        String b = "dee";
+        String a = "JUHARIANA";
+        String b = "juhariana";
 
         System.out.println(a.equalsIgnoreCase(b)); 
-	}
+   }
 }
