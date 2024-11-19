3eb064ec4d00e380a9e48329d45c37c4d8a2c56b
Juhariana <juharianaana@gmail.com>
2024-11-19 21:16:22 +0800

    String method : toString

---
+++ Day79.java
@@ -0,0 +1,16 @@
+public class Day79 {
+	public static void main(String[] args) {
+			
+			byte a = 25 ;
+			int b = 100 ;
+			boolean c = true ;
+			
+		String d = Byte.toString (a);
+		String e = Integer.toString (b);
+		String f = Boolean.toString (c);
+		
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+	}
+}
