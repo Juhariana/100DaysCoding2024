c43f87a37f0a93b9223c98afba27544cad521296
Juhariana <juharianaana@gmail.com>
2024-11-20 20:59:35 +0800

    String method : trim

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class Day80{
+    public static void main(String[] args) {
+        String nama1= "    juhariana   ";
+        String nama2= nama1.trim();
+      
+      System.out.println("nama saya sebelum di trim : " + nama1);
+        System.out.println("nama saya setelah di trim : " + nama2 );
+    }
+}
