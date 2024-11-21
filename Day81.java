3becf13e67234f651bab114d3fdd2d3766a079c5
Juhariana <juharianaana@gmail.com>
2024-11-21 19:17:40 +0800

    String method : valueof

---
+++ Day81.java
@@ -0,0 +1,10 @@
+public class Day81{
+    public static void main(String[] args) {
+        int a = 42;
+  
+        String b= String.valueOf(a);
+       
+        System.out.println("setelah dikonversi " + b);
+        
+    }
+}
