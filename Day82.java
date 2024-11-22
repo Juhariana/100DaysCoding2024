6a323d1e831ef711203268d994014fea33a54592
Juhariana <juharianaana@gmail.com>
2024-11-22 21:11:54 +0800

    Method : ceil(),floor(),round()

---
+++ Day82.java
@@ -0,0 +1,11 @@
+public class Day82 {
+    public static void main(String[] args) {
+
+        double angka = 7.4;
+         
+        System.out.println("Angka asli: " + angka);
+        System.out.println("pembulatan ke ceil : " + Math.ceil(angka));
+        System.out.println("Pembulatan ke floor : " + Math.floor(angka));
+        System.out.println("Pembulatan ke round: " + Math.round(angka));
+    }
+}
