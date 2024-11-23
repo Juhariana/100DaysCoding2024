0d9796e7c8edea7a48e1655b1b6a7e1117d8430e
Juhariana <juharianaana@gmail.com>
2024-11-23 19:11:37 +0800

    Array satu dimensi

---
+++ Day83.java
@@ -0,0 +1,10 @@
+public class Day83{
+    public static void main(String[] args) {
+     
+        char[] nama = {'J', 'U', 'H', 'A', 'R', 'I', 'A', 'N', 'A'};
+       
+        for (int i = 0; i < nama.length; i++) {
+            System.out.print(nama[i]);
+        }
+    }
+}
