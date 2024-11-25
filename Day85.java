6881e4cf8fd737119c62fba402a73a41e760b589
Juhariana <juharianaana@gmail.com>
2024-11-25 22:08:46 +0800

    Mengisi elemen ke array satu dimensi pada saat diinisialisasi

--- Day85.java
+++ Day85.java
@@ -1,14 +1,11 @@
-public class day85 {
-    public static void main(String[] args) {
-        char[] Huruf = new char[5];
+public class day {
+	public static void main(String[] args) {
+		char[]  Huruf = new char[5];
         Huruf[0] = 'a';
         Huruf[1] = 'i';
         Huruf[2] = 'u';
         Huruf[3] = 'e';
         Huruf[4] = 'o';
-
-        for (int i = 0; i < Huruf.length; i++) {
-            System.out.println(Huruf[i]);
-        }
-    }
+        System.out.println(Huruf);
+	}
 }
