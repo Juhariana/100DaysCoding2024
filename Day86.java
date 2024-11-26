17f3746522cdb3116c8d7889f4ef03e6c3205256
Juhariana <juharianaana@gmail.com>
2024-11-26 20:14:33 +0800

    Mencetak elemen pada array satu dimensi

---
+++ Day86.java
@@ -0,0 +1,10 @@
+public class Day86{
+    public static void main(String[] args) {
+   String[] Buah = {"apel", "semangka", "pisang", "mangga", "jeruk"};
+
+        for (String buah : Buah) {
+        //Mencetak elemen-elemen array
+            System.out.println(buah);
+        }
+    }
+}
