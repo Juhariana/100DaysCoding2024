df83efa0c9ea56eac0a4422de1fad5cd0ac5797f
Juhariana <juharianaana@gmail.com>
2024-11-27 18:42:06 +0800

    Mengubah nilai array satu dimensi ke indeks tertentu

---
+++ Day87.java
@@ -0,0 +1,9 @@
+public class Day87 {
+    public static void main(String[] args) {
+        String[] nama = {"juha", "ana"};
+        nama[1] = "riana"; 
+        for (String Nama : nama) {
+            System.out.print(Nama + " ");
+        }
+    }
+}
