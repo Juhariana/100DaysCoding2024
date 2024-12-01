2145abb09b54a88a82d7fdfc782a255d34340160
Juhariana <juharianaana@gmail.com>
2024-12-01 21:42:15 +0800

    Soal percabangan: you and i , end

--- Day91.java
+++ Day91.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
 
 public class Soal4 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         while (true) {
             System.out.print("Masukkan angka: ");
-            try {
+            
+            if (input.hasNextInt()) {
                 int angka = input.nextInt();
-                
+
                 if (angka % 2 == 0) {
                     System.out.println("Genap");
                 } else {
                     System.out.println("You and I, end");
                     break; 
                 }
-            } catch (Exception e) {
-                System.out.println("Masukkan hanya angka.");
+            } else {
+                System.out.println("Masukkan hanya angka"); 
                 input.next(); 
             }
         }
-         input.close();
+
+        input.close(); 
     }
 }
